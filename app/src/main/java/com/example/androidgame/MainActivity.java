9e77f417e48308697fec8811777217221dde16ea
Sieun Lim <lim2024sieun@gmail.com>
2024-06-03 18:56:06 +0900

    bonus 함수 생성

--- app/src/main/java/com/example/androidgame/MainActivity.java
+++ app/src/main/java/com/example/androidgame/MainActivity.java
@@ -19,18 +19,22 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private static int diceFace;
     private int[] images;
+    private int[][] location;
+    private String[] event;
     private static int turn;//0은 유저 1은 컴퓨터
-    private static int player;//말 위치
-    private static int computer;//말 위치
+    private static int playerLocation;//말 칸 위치
+    private static int computerLocation;//말 칸 위치
     private static int playerScore;//플레이어 점수
     private static int computerScore;//컴퓨터 점수
-    private static boolean playerCnt;//한바퀴 돌고 보너스 받을거 있는지
-    private static boolean computerCnt;//한바퀴 돌고 보너스 받을거 있는지
+    private static boolean playerBonus;//한바퀴 돌고 보너스 받을거 있는지
+    private static boolean computerBonus;//한바퀴 돌고 보너스 받을거 있는지
     private TextView palyerScoreV;
     private TextView computerScoreV;
     private ImageView palyerMarker;
     private ImageView computerMarker;
-
+    public static final int BONUSCOIN=5; //한 바퀴 돌면 받는 보너스 코인의 수\
+    public static final int EVENTNUM=7;//이벤트 종류 수
+    public static final int EVENT1=5; //이벤트 있는 칸 번호
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,8 +58,19 @@ public class MainActivity extends AppCompatActivity {
         //다이스 초기값
         diceFace=1;
 
-        //말판 번호 배열
-        //int[] a= new int[33];
+//        event=new String[EVENTNUM];
+//        event= new String[]{
+//                "당신은 금화가 담긴 주머니를 발견했다! \n ",
+//                "",
+//                "",
+//                "",
+//                "",
+//                "",
+//                ""
+//
+//        };//대사,(표정)
+        //칸 별 좌표 값 =>이거 대신 말이 있는 이미지 없는 이미지로 할까 고민
+        location= new int[20][2];
 
     }
 
@@ -65,43 +80,51 @@ public class MainActivity extends AppCompatActivity {
         diceFace = random.nextInt(6);
         dice.setImageResource(images[diceFace]);//주사위 이미지 바꿔줌
 
-        if (turn == 0) {
-            player += diceFace;//주사위 굴린 만큼 증가
+        if (turn == 0) {//유저의 순서면
+            playerLocation += diceFace;//주사위 굴린 만큼 증가
 
-            if (player >= 33) {//한바퀴 돌았을때
-                player -= 33;
-                playerCnt=true;//한 바퀴 돌면 보너스 금화 받을거
+            if (playerLocation >= 33) {//한바퀴 돌았을때
+                playerLocation -= 33;
+                bonus(playerScore); //한 바퀴 돌았으니 보너스 금화 받기
             }
-            move(turn);
+            move(playerLocation);
             turn=1;
         }else {
-            computer += diceFace;//주사위 굴린 만큼 증가
+            computerLocation += diceFace;//주사위 굴린 만큼 증가
 
-            if (computer >= 33) {//한바퀴 돌았을때
-                computer -= 33;
-                computerCnt=true;//한 바퀴 돌면 보너스 금화 받을거 => 굳이 변수 만들지 말고 바로 증가시키는 함수 만들어서 불러줘도 좋을듯
+            if (computerLocation >= 33) {//한바퀴 돌았을때
+                computerLocation -= 33;
+                bonus(computerScore); //한 바퀴 돌았으니 보너스 금화 받기
             }
-            move(turn);
+            move(computerLocation); //말(이미지 뷰) 위치 이동
+
+
             turn=0;
         }
 
+
     }
-    public static void move(int turn) {
+    public static void move(int userLocation) {
         if (turn == 0) {
-            switch(player) {
-                case 5://이벤트가 있는 칸이면 이벤트 보여줌...
+            //maker(말) 이동해주는....
 
+
+            switch(userLocation) {//유저 말 위치가
+                case EVENT1://이벤트가 있는 칸이면 이벤트 보여줌...
+                    event(EVENT1);//이벤트 발생
                     break;
-
             }
-            //maker(말) 이동해주는 코드... => 이것저것 하려면 매개변수랑 이것저것 수정해야할듯
-
-            if(playerCnt) {//보너스 받을 거 있으면...여기서 말고 그냥 따로 함수 만들어도 좋을듯...
 
 
-            }
         }else {
 
         }
+    }
+    public static void bonus(int userScore){
+        userScore+=BONUSCOIN;
+    }
+
+    public static void event(int EVENTNUM){
+
     }
 }
